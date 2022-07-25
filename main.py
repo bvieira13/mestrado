e73fcd4fab71b10cfbc3d2b6095530bfcacecbbb
Bruno Freitas Vieira <b.vieira@animalltag.com.br>
2022-07-25 14:49:58 -0300

    Implementado média de uma região de interesse e salvamento automático

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import imfun as im
 import matplotlib.pyplot as plt;
+import shutil
 
 def equalize_histogram(img):
   # Computando o valo máximo e mínimo dos niveis de cinza presentes na imagem
@@ -12,9 +13,9 @@ def equalize_histogram(img):
   img_out = img_norm.astype(np.uint8);
   return img_out
 
-def espectral_roi(hsi_img, index = 25):
+def espectral_square_roi(hsi_img, index = 25):
     
-    ref_img_roi, points = im.polyroi(hsi_img[index])
+    [img_mask, points] = im.polyroi(hsi_img[index])
 
     dim_x = [];
     dim_y = [];
@@ -39,29 +40,62 @@ def espectral_roi(hsi_img, index = 25):
     
     return vec_espctral;
 
+def espectral_roi(hsi_img, index = 23):
+    
+    [img_mask, points] = im.polyroi(hsi_img[index]);
+    img_mask = img_mask[:,:];
+    img_mask[img_mask>0] = 1;
+
+    vec_espctral = [];
+
+    for m in range(0, len(hsi_img)):
+        img_temp = img_mask*hsi_img[m][:,:];
+        mean = np.mean(img_temp[img_temp>0]);
+        vec_espctral.append(mean);
+    
+    return vec_espctral;
+
 def main():
     current_dir = os.getcwd();
     wavelength = np.linspace(400,720,33);
 
-    img_dir = current_dir + '\Hand';
-    ref_dir = current_dir + '\Reference';
+    img_dir = current_dir + '\dataset\Hand';
+    ref_dir = current_dir + '\dataset\Reference';
 
     hsi_img = im.load_gray_images(img_dir);
     hsi_ref = im.load_gray_images(ref_dir);    
 
-    ref_espectral = espectral_roi(hsi_ref,15);
-    img_espectral = espectral_roi(hsi_img,15);
+    ref_espectral = espectral_square_roi(hsi_ref);
+    img_espectral = espectral_roi(hsi_img);
 
 
     white_espectral = np.divide(ref_espectral, ref_espectral); 
     hand_espectral = np.divide(img_espectral, ref_espectral);
+    hand_norm = [];
+
+    for k in range(0, len(hsi_img)):
+        norm = hsi_img[k][:,:]/ref_espectral[k];
+        hand_norm.append(norm);
+
+    file_name = 'hsi_reflectance_norm.png'
+    dst_folder = current_dir + '\\results\\';
+    scr_folder = current_dir + '\\';
 
     plt.figure(figsize=(5,5));
-    plt.plot(wavelength, white_espectral, '-b', label="Reflectância de branco puro");
-    plt.plot(wavelength, hand_espectral, '-r', label="Reflectância de pele");
-    plt.title("Curva espectral de reflectância");
+    plt.semilogy(wavelength, white_espectral, '-b', label='Branco');
+    plt.semilogy(wavelength, hand_espectral, '-r', label='Tecido cutâneo');
+    plt.title('Curva espectral de tecido cutâneo');
+    plt.xlabel('Comprimento de onda (nm)');
+    plt.ylabel('Reflectância normalizada')
     plt.legend();
+    plt.savefig(file_name);
     plt.show();
 
+    if os.path.exists(dst_folder + file_name):
+        path = dst_folder + file_name;
+        os.remove(path)
+        shutil.move(scr_folder + file_name, dst_folder + file_name)
+    else:
+        shutil.move(scr_folder + file_name, dst_folder + file_name)
 
 main()
